a5aad84d502844cbf3e0ca7b7af1ecfe188f3d0d
ymd65536 <ymd65536@gmail.com>
2024-03-06 10:20:46 +0900

    add: text_splitter

--- ai_app_file_loader/app.py
+++ ai_app_file_loader/app.py
@@ -3,6 +3,9 @@ import os
 from slack_bolt import App, Ack
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 
+from langchain_community.document_loaders import DirectoryLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
 project_id = os.environ.get("PROJECT_ID", "")
 APP_ENVIRONMENT = os.environ.get("APP_ENVIRONMENT", "")
 SLACK_BOT_TOKEN = os.environ.get("SLACK_BOT_TOKEN", "")
@@ -14,6 +17,29 @@ app = App(
 )
 
 
+def document_loader():
+    loader = DirectoryLoader('./', glob="**/README.md")
+    return loader.load()
+
+
+def text_splitter(docs, chunk_size=1000, chunk_overlap=100, separators=["\n\n", "。"]):
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=chunk_size,
+        chunk_overlap=chunk_overlap,
+        separators=separators
+    )
+
+    split_text = []
+
+    for doc in docs:
+        if len(doc.page_content) > chunk_size:
+            split_text.extend(text_splitter.split_documents(docs))
+        else:
+            split_text.extend(doc)
+
+    return split_text
+
+
 def handle_mention(event, say):
     print("handle_mention")
     thread_id = event['ts']
@@ -21,7 +47,11 @@ def handle_mention(event, say):
     if "thread_ts" in event:
         thread_id = event['thread_ts']
 
-    say(event['text'], thread_ts=thread_id)
+    docs = document_loader()
+    split_text = text_splitter(docs)
+
+    res = f"分割したテキストの数：{len(split_text)}"
+    say(res, thread_ts=thread_id)
 
 
 def slack_ack(ack: Ack):
